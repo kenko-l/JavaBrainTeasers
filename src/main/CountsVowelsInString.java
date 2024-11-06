1a340369cf9ba6389db0d691e68c6994ef14ef3b
kenko-l <kenko.latchman@gmail.com>
2024-11-06 11:29:36 -0500

    feat: add program that counts number of vowels in a string

---
+++ src/main/CountsVowelsInString.java
@@ -0,0 +1,23 @@
+package main;
+import java.util.Scanner;
+public class CountsVowelsInString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		int count = 0;
+		
+		for(int i = 0; i < input.length(); i++) {
+			char vowel = Character.toLowerCase(input.charAt(i));
+			
+			if(vowel == 'a' || vowel == 'e' || vowel == 'i' || vowel == 'o' || vowel == 'u') {
+				count++;
+			}
+		}
+		
+		System.out.println("You have " + count + "vowels in your word");
+		
+
+	}
+
+}
